332f5c0f64828995368cbcf008e112c905926cb5
Thuvarakan Sritharan <sthuva98@gmail.com>
2024-12-15 17:03:56 +0530

    Fix connector display name issue

--- org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/customservice/synapse/connectors/ConnectorReader.java
+++ org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/customservice/synapse/connectors/ConnectorReader.java
@@ -53,10 +53,12 @@ public class ConnectorReader {
                     DOMNode componentElement = Utils.getChildNodeByName(connectorElement, "component");
                     DOMNode displayNameElement = Utils.getChildNodeByName(componentElement, "displayName");
                     String name = componentElement.getAttribute(Constant.NAME);
-                    String displayName = Utils.getInlineString(displayNameElement.getFirstChild());
                     connector = new Connector();
                     connector.setName(name);
-                    connector.setDisplayName(displayName);
+                    if (displayNameElement != null && displayNameElement.isElement()) {
+                        String displayName = Utils.getInlineString(displayNameElement.getFirstChild());
+                        connector.setDisplayName(displayName);
+                    }
                     connector.setPath(connectorPath);
                     connector.setVersion(getConnectorVersion(connectorPath));
                     connector.setIconPath(connectorPath + File.separator + "icon");
