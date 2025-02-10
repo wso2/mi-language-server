9aa0d73f8810ebcc2282ee0ab6aea53066b4317d
chathurangaj <chathuranga.jayanath.1999@gmail.com>
2025-02-10 22:48:33 +0530

    remove redundant null check

--- org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/customservice/synapse/directoryTree/DirectoryTreeBuilder.java
+++ org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/customservice/synapse/directoryTree/DirectoryTreeBuilder.java
@@ -337,7 +337,7 @@ public class DirectoryTreeBuilder {
         String registryPath = projectPath + File.separator + Constant.SRC + File.separator +
                 MAIN + File.separator + WSO2MI + File.separator + RESOURCES;
         File folder = new File(registryPath);
-        if (folder != null && folder.exists()) {
+        if (folder.exists()) {
             if (!folder.isHidden()) {
                 String folderName = folder.getName();
                 FolderNode resourceFolderNode = new FolderNode(folderName, registryPath);
