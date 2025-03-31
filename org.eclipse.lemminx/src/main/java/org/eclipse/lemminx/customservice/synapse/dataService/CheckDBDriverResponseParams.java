262d842a99242ebda5a5032e6ad0cca2e31b4702
Chinthaka Jayatilake <37581983+ChinthakaJ98@users.noreply.github.com>
2025-03-31 22:27:09 +0530

    Add setter and getter methods

--- org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/customservice/synapse/dataService/CheckDBDriverResponseParams.java
+++ org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/customservice/synapse/dataService/CheckDBDriverResponseParams.java
@@ -29,4 +29,12 @@ public class CheckDBDriverResponseParams {
         this.driverVersion = driverVersion;
         this.driverPath = driverPath;
     }
+
+    public boolean isDriverAvailable() {
+        return isDriverAvailable;
+    }
+
+    public String getDriverVersion() {
+        return driverVersion;
+    }
 }
